f5ce34af2036192fa7227b761739fcbc4773231f
Prakash Kumar Jena <youremail@example.com>
2026-05-11 23:48:13 +0530

    init setup

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/public/Home";
+import About from "./pages/public/About";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
